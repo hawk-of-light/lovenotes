95b46750ad83ca506adb2b1d279cf70279ac386b
dream seeker <hawkoflight5@gmail.com>
2024-03-15 22:57:17 -0700

    Small progress 3/15/24
    The capability of points being drawn to the screen has been
    reintroduced. In a slightly, if I'm not mistaken, more efficient manner
    than before.

--- main.lua
+++ main.lua
@@ -1,19 +1,36 @@
--- [Constants] --
-
--- Window Settings:
-windowWidth, windowHeight = love.window.getMode()
-
 -- [Main Functions] --
 
--- Loads all the related components of the program
+
 function love.load()
+-- Loads all the related components of the program
+
+    -- Draw settings --
+    drawSize = 5
+    love.graphics.setPointSize(drawSize)
+
+    drawnPoints = {}
+
+    -- Window Settings --
+    windowWidth, windowHeight = love.window.getMode()    
+    
 
 end
 
 function love.update()
 
+    if love.mouse.isDown(1) then
+        drawnPoints[#drawnPoints+1] = {love.mouse.getX(), love.mouse.getY()}
+    end
+
 end
 
 function love.draw()
 
+    for k, v in pairs(drawnPoints) do
+        love.graphics.points(v[1], v[2])
+    end    
+    
+
 end
+
+
