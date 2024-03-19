983a01aed6637dd707ff246b33785752ccdb6cb0
dream seeker <hawkoflight5@gmail.com>
2024-03-18 22:58:28 -0700

    Reintroduced linear interpolation
    The project is now back to where I had originally left it off at...

--- main.lua
+++ main.lua
@@ -22,12 +22,20 @@ end
 function love.update()
 -- continuously updates the state of the world x frames per second
 
-    -- Adds a points cooridanates (mouse xy posn on canvas) to a list of points to be drawn on left click 
-    -- The list format is necessary to allow them to remain drawn each frame
+    --[[ Adds a point's cooridanates (mouse xy posn on canvas) to a list of points to be drawn on left click 
+         The list format is necessary to allow them to remain drawn each frame 
+         Additionally, calls addInterpolatedPoints to create smoothness in the drawing --]]
     if love.mouse.isDown(1) and drawMode then
         drawnPoints[#drawnPoints+1] = {love.mouse.getX(), love.mouse.getY()}
+        if #drawnPoints > 0 then
+            local lastPoint = drawnPoints[#drawnPoints]
+            local currentPoint = {love.mouse.getX(), love.mouse.getY()}
+            addInterpolatedPoints(lastPoint, currentPoint)
+        end
     end
 
+    --[[ On left click, removes any points within radius = eraseSize of the mouse position (ex, ey) 
+         from the list of points to be drawn onto the screen --]]
     if love.mouse.isDown(1) and not drawMode then
         local ex, ey = love.mouse.getPosition()
         for i = #drawnPoints, 1, -1 do
@@ -48,6 +56,8 @@ function love.draw()
         love.graphics.points(v[1], v[2])
     end    
     
+    -- FPS counter for diagnostics
+    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
 
 end
 
@@ -58,3 +68,16 @@ function love.mousepressed(x, y, button)
     end
 end
 
+--[[ Adds points between the last point drawn and the current point drawn using linear interpolation to 
+     attempt to reproduce smoothness]]
+function addInterpolatedPoints(pointA, pointB)
+    local distance = math.sqrt((pointB[1] - pointA[1])^2 + (pointB[2] - pointA[2])^2)
+    local numPoints = math.floor(distance / drawSize) -- Adjust this based on drawSize for better smoothness
+
+    for i = 1, numPoints do
+        local t = i / numPoints
+        local x = pointA[1] * (1 - t) + pointB[1] * t
+        local y = pointA[2] * (1 - t) + pointB[2] * t
+        drawnPoints[#drawnPoints + 1] = {x, y}
+    end
+end
